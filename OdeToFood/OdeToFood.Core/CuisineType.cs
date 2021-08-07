62819250123bc59a10dd09a8bc5eba8adcfbe127
Davorin <davorin.jurkovic@gmail.com>
2021-08-07 23:49:56 +0200

    Drilling into Data

---
+++ OdeToFood/OdeToFood.Core/CuisineType.cs
@@ -0,0 +1,11 @@
+﻿namespace OdeToFood.Core
+{
+    public enum CuisineType
+    {
+        None,
+        Mxican, 
+        Italian,
+        Indian,
+        Croatian
+    }
+}
---
+++ OdeToFood/OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian },
+                new Restaurant {Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.Italian },
+                new Restaurant {Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Mxican},
+                new Restaurant {Id = 4, Name = "Kod Medveda", Location = "Arizona", Cuisine = CuisineType.Indian },
+                new Restaurant {Id = 5, Name = "Vith George", Location = "Maryland", Cuisine = CuisineType.Croatian },
+                new Restaurant {Id = 6, Name = "Blentava Glava", Location = "Maryland", Cuisine = CuisineType.Croatian },
+
+            };
+        }
+
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(o => o.Name).ToList();
+        }
+    }
+}
